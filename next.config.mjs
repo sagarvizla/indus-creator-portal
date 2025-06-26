97318ccb8ae75da1f179966e25a82e05669b3dca
sagarvizla <sagarvizla@gmail.com>
2025-06-26 09:55:48 +0530

    Fix: Correct syntax in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // Tell Next.js to ignore all ESLint errors during the production build
   eslint: {
     ignoreDuringBuilds: true,
